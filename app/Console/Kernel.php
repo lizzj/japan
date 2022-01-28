f88b8bb1df4c1bad5b0f132baeb2eba735a217a3
lizzj <li_zzj@163.com>
2022-01-28 17:22:16 +0800

    first
    Signed-off-by: lizzj <li_zzj@163.com>

---
+++ app/Console/Kernel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console;
+
+use App\Jobs\Auth\User\User\Reset;
+use App\Jobs\System\Remind\Remind;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->job(new Remind)->everyMinute();//每分钟执行一次
+        $schedule->job(new Reset)->daily();//每天0点执行
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
